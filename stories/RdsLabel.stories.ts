ce5335c643ad58aa27afa084d109a3d9d7e4740c
marlepo <marcinlpotrykus@gmail.com>
2025-06-19 14:52:21 +0200

    fix: stories component incorrect file name

--- stories/RdsLabel.stories.ts
+++ stories/RdsLabel.stories.ts
@@ -21,7 +21,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Gray: Story = {
   args: {
-    _intent: "gray",
     children: "Placeholder",
   },
 };
